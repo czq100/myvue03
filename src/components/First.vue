b9c1e88a62fc3d14129f1cf1ffde974844fca94c
程志强 <zhiqiang.cheng@zhaopin.com.cn>
2021-02-05 19:54:53 +0800

    3333

--- src/components/First.vue
+++ src/components/First.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>ssssssssssss</div>
+  <div @click="getIndustry">ssssssssssss</div>
   <span>{{msg}}</span>
 </template>
 
 <script>
+
 export default {
   props: {
     msg: {
       type: String,
+    },
+  },
+  methods: {
+    getIndustry() {
+      var _url =
+        "http://api.open.zhaopin.com/dict/dictService/getItemList?access_token=c4215a32bbf74a2aa410db8473a58f4dV8k5&dictName=industry_relation&sceneName=";
+      this.axios
+        .get(_url)
+        .then((resp) => {
+          console.log(resp);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 };
