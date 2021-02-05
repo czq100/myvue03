f0ad71efbda6b99762978e7229e2666db67efafc
程志强 <zhiqiang.cheng@zhaopin.com.cn>
2021-02-05 18:54:14 +0800

    1111

---
+++ src/components/First.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>ssssssssssss</div>
+  <span>{{msg}}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
