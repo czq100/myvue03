0f381cfdd08c527b00ed5410c9a5b98225650aec
程志强 <zhiqiang.cheng@zhaopin.com.cn>
2021-02-17 22:05:29 +0800

    2222

--- src/components/First.vue
+++ src/components/First.vue
@@ -1,15 +1,27 @@
 <template>
   <div @click="getIndustry">ssssssssssss</div>
   <span>{{msg}}</span>
+  <br />
+  <input type="text" v-model="keyword" @keyup="getIndustry2" />
+  <ul>
+    <li v-for="(item,index) in list" :key="index">{{item.q}}</li>
+  </ul>
 </template>
 
 <script>
-
+import fetchjsonp from "fetch-jsonp";
 export default {
   props: {
     msg: {
       type: String,
     },
+  },
+  data() {
+    return {
+      keyword: "",
+      list: [],
+      timer: "",
+    };
   },
   methods: {
     getIndustry() {
@@ -23,6 +35,30 @@ export default {
         .catch((error) => {
           console.log(error);
         });
+    },
+    getIndustry2() {
+      //debugger;
+      if (!this.keyword || !this.keyword.trim()) {
+        return;
+      }
+
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        var _url =
+          "https://www.baidu.com/sugrec?prod=pc&wd=" +
+          encodeURIComponent(this.keyword);
+        fetchjsonp(_url, { timeout: 5000, jsonpCallback: "cb" })
+          .then((resp) => {
+            return resp.json();
+          })
+          .then((data) => {
+            console.log(data);
+            this.list = data.g;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }, 500);
     },
   },
 };
